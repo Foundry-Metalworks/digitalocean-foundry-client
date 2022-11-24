6675f7be882bf8f5d22c789cd0606aca0a03afc5
Tenzin Pelletier <pelletiertenzin2000@gmail.com>
2022-11-24 11:01:01 +0100

    basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,95 @@
+import React, { ReactNode } from 'react';
 import './App.css';
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+const ORCHESTRATOR_URL = 'https://dnd-orchestrator.t2pellet.me';
+const FOUNDRY_URL = 'https://foundry.t2pellet.me';
+const getUrl = (path: string) => ORCHESTRATOR_URL + `/api/${path}`;
+
+type State = {
+  loaded: boolean;
+  serverStatus: string;
+  serverIP: string;
+};
+
+type Props = {};
+
+class App extends React.Component<Props, State> {
+  constructor(props: never) {
+    super(props);
+    this.state = {
+      loaded: false,
+      serverStatus: 'off',
+      serverIP: FOUNDRY_URL
+    };
+  }
+
+  private async getStatus() {
+    const result = await fetch(getUrl('status'));
+    return await result.json();
+  }
+
+  private async getIP() {
+    let url;
+    try {
+      const reachableCheck = await fetch(FOUNDRY_URL, { mode: 'no-cors' });
+      if (reachableCheck.status != 404) url = FOUNDRY_URL;
+    } finally {
+      if (!url) {
+        const result = await fetch(getUrl('ip'));
+        const ip = (await result.json()).ip;
+        url = `http://${ip}:30000`;
+      }
+    }
+    return url;
+  }
+
+  private async startFoundry() {
+    const options = {
+      method: 'POST'
+    };
+    await fetch(getUrl('start'), options);
+  }
+
+  async componentDidMount(): Promise<void> {
+    let statusResponse = await this.getStatus();
+    if (statusResponse.status === 'deleted') {
+      this.setState({ loaded: true, serverStatus: statusResponse.status });
+      await this.startFoundry();
+      statusResponse = await this.getStatus();
+    }
+    const serverIP = await this.getIP();
+    this.setState({ loaded: true, serverStatus: statusResponse.status, serverIP });
+  }
+
+  render(): ReactNode {
+    const { loaded, serverStatus, serverIP } = this.state;
+
+    console.log('Loaded: ' + loaded);
+    console.log('Status: ' + serverStatus);
+    console.log('IP: ' + serverIP);
+
+    if (loaded) {
+      if (serverStatus === 'active') {
+        window.location.replace(serverIP);
+        return (
+          <div className="App">
+            <p>Redirecting...</p>
+          </div>
+        );
+      }
+      return (
+        <div className="App">
+          <p>Server is Starting...</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <p>Loading...</p>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
