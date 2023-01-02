c8aceb30275f90bee43d02ba3a8deb2939207226
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2023-01-02 04:33:21 +0100

    fix home page auth error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,14 @@ import { useNavigate } from 'react-router';
 
 const ORCHESTRATOR_URL = 'https://dnd-orchestrator.t2pellet.me';
 const getApiUrl = (path: string) => ORCHESTRATOR_URL + `/api/${path}`;
+const headers: Headers = new Headers({
+  Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`
+});
 
 export default function Home(): React.ReactElement {
   const navigate = useNavigate();
   const checkExists = async () => {
-    const result = await fetch(getApiUrl(import.meta.env.VITE_NAME));
+    const result = await fetch(getApiUrl(import.meta.env.VITE_NAME), { headers });
     const value = await result.json();
     return value.exists;
   };
