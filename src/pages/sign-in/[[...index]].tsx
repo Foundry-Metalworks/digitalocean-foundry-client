9e4db8e73aa317dd8512f10aa99899cb56239b51
Tenzin Pelletier <tenzin.pelletier@vividseats.com>
2023-04-26 13:03:31 -0400

    bigger sign-up / sign-in link for sign-in / sign-up

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -3,7 +3,16 @@ import React from 'react'
 import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = (): React.ReactNode => {
-    return <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+    return (
+        <SignIn
+            path="/sign-in"
+            routing="path"
+            signUpUrl="/sign-up"
+            appearance={{
+                elements: { footerActionText: { fontSize: '1rem' }, footerActionLink: { fontSize: '1rem' } },
+            }}
+        />
+    )
 }
 
 export default SignInPage
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -3,7 +3,16 @@ import React from 'react'
 import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = (): React.ReactNode => {
-    return <SignUp path="/sign-up" routing="path" afterSignUpUrl="/setup" />
+    return (
+        <SignUp
+            path="/sign-up"
+            routing="path"
+            afterSignUpUrl="/setup"
+            appearance={{
+                elements: { footerActionText: { fontSize: '1rem' }, footerActionLink: { fontSize: '1rem' } },
+            }}
+        />
+    )
 }
 
 export default SignUpPage
