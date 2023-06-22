2ab852158472d56ad69cd8ae6e80715a01d69254
Tenzin Pelletier <tenzin.pelletier@vividseats.com>
2023-06-22 16:14:56 -0400

    fix: fixup do auth

--- src/pages/digitalocean/index.tsx
+++ src/pages/digitalocean/index.tsx
@@ -5,17 +5,21 @@ import { useRouter } from 'next/router'
 
 import Loading from '@/components/kit/loading'
 import { useUser } from '@/hooks/use-user'
+import { RedirectToSignIn, useAuth } from '@clerk/nextjs'
 
 const DigitalOcean: NextPage = () => {
-    const { query } = useRouter()
+    const { query, asPath } = useRouter()
+    const { isSignedIn } = useAuth()
     const {
         actions: { authorize },
     } = useUser()
     const code = query.code as string
 
     useEffect(() => {
-        if (!!code) authorize(code)
-    }, [code])
+        if (isSignedIn && !!code) authorize(code)
+    }, [code, isSignedIn])
+
+    if (isSignedIn == false) return <RedirectToSignIn redirectUrl={asPath} />
 
     return <Loading />
 }
